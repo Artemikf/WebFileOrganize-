059e985ce58e8ef39ac273f52ede3ce8edae1a1b
Artem Kurta <artemdog386@gmail.com>
2025-05-31 23:32:57 +0300

    make main and space pages

---
+++ Main/Pages/Space/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Main.Pages.Space
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
